42f20a907d80760758c9713836b462ae768f9415
matilda <over2505@gmail.com>
2025-02-23 22:59:30 +0700

    update

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:test_calendar/pages/today_page.dart';
+import 'package:test_calendar/provider/calendar_time_provider.dart';
+import 'package:test_calendar/provider/time_provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+MaterialColor white = const MaterialColor(
+  0xFFFFFFFF,
+  <int, Color>{
+    50: Color(0xFFFFFFFF),
+    100: Color(0xFFFFFFFF),
+    200: Color(0xFFFFFFFF),
+    300: Color(0xFFFFFFFF),
+    400: Color(0xFFFFFFFF),
+    500: Color(0xFFFFFFFF),
+    600: Color(0xFFFFFFFF),
+    700: Color(0xFFFFFFFF),
+    800: Color(0xFFFFFFFF),
+    900: Color(0xFFFFFFFF),
+  },
+);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create:(_) => TimeProvider()),
+        ChangeNotifierProvider(create: (_) => SelectedTimeChangeProvider())
+      ],
+      child: ScreenUtilInit(
+          designSize: const Size(360, 690),
+          minTextAdapt: true,
+          splitScreenMode: true,
+          builder: (context , child) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(primarySwatch: Colors.grey),
+              home: const TodayPage(),
+            );
+          }
+      ),
+    );
+  }
+}
